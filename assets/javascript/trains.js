70713944151fb60a9fdf3fbfae3ce5f30ccb4532
Katherine Rinas <katherinerinas@Katherine-Rinass-MacBook-Pro.local>
2018-02-02 19:47:25 -0700

    added css index and some basic styling as well as the link to firebase

---
+++ assets/javascript/trains.js
@@ -0,0 +1,14 @@
+
+  var config = {
+    apiKey: "AIzaSyAueOzPrIK2N_xM55izJ6OK60JrFPX7YIs",
+    authDomain: "my-awesome-project-34a06.firebaseapp.com",
+    databaseURL: "https://my-awesome-project-34a06.firebaseio.com",
+    projectId: "my-awesome-project-34a06",
+    storageBucket: "my-awesome-project-34a06.appspot.com",
+    messagingSenderId: "543373788237"
+  };
+  firebase.initializeApp(config);
+
+  firebase.database();
+
+  
